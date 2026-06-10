9a650f587a7fd455687d2453ef2465b5e2f59c9e
isaque tornquist <isaque.tornquist@escola.pr.gov.br>
2026-06-10 16:30:30 +0000

    aula de navegação de telas

--- aula-pm/src/app/index.tsx
+++ aula-pm/src/app/index.tsx
@@ -1,9 +1,14 @@
-import { Text, View, StyleSheet } from "react-native";
+import {router} from "expo-router":
+import { Text, View, StyleSheet, Touchable, TouchableOpacicty } from "react-native";
 
 export default function Index() {
   return (
     <View style={styles.container}>
-      <Text>Edit src/app/index.tsx to edit this screen.</Text>
+      <Text style={styles.titulo}>HOME </Text>
+      <Text style={styles.subtitulo}>Bem vindo ao nosso APP!</Text>
+      <TouchableOpacicty style={styles.Botao} onPrass={router.push('/detalhes')}>
+      <Text style={styles.textoBotao}><Detalhes></Text>
+      </TouchableOpacicty>
     </View>
   );
 }
